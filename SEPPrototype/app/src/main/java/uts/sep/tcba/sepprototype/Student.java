93f6bbbf0ca553b19d068a0cbfddbd4b21af510a
sc1122 <sean.crimmins97@gmail.com>
2017-09-12 12:50:26 +1000

    More initialisation code for the student

--- SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Student.java
+++ SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Student.java
@@ -1,13 +1,16 @@
 package uts.sep.tcba.sepprototype;
 
+import java.util.LinkedList;
+
 public class Student extends User {
 
-
     private User student;
+    private LinkedList<Integer> studentSubjects;
 
 
     public Student(User user) {
         student = Student.this;
+        studentSubjects = Student.this.getSubjects();
     }
 
     public void selectTimeslot() {
@@ -19,8 +22,7 @@ public class Student extends User {
     }
 
     public void viewTutAvailability() {
-        student.getSubjects();
-
+        
     }
 
 }
