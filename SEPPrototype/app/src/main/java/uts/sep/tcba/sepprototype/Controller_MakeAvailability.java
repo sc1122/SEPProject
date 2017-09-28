ebbf97cbe0a61f1ff34875770cd300a9b31dc098
Liam <jinglongh@yahoo.com>
2017-09-28 14:41:12 +1000

    Add controller for indicating Availability

---
+++ SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Controller_MakeAvailability.java
@@ -0,0 +1,13 @@
+package uts.sep.tcba.sepprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Controller_MakeAvailability extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_availability);
+    }
+}
