c734644900993d885fa53d679bbe07df22421111
Villiam Von Doom <jinglongh@yahoo.com>
2017-09-28 17:04:53 +1000

    change to 24hour format

--- SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Controller_MakeAvailability.java
+++ SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Controller_MakeAvailability.java
@@ -2,12 +2,22 @@ package uts.sep.tcba.sepprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
 
 public class Controller_MakeAvailability extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_availability);
+
+        TimePicker startTime = (TimePicker) findViewById(R.id.startTime);
+        startTime.setIs24HourView(true);
+
+        TimePicker endTime = (TimePicker) findViewById(R.id.endTime);
+        endTime.setIs24HourView(true);;
     }
 }
