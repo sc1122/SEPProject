315bfea00e60c5062fb445d8e1344356f1a3f3ca
Caleb Adra <caleb.adra@gmail.com>
2017-10-15 19:20:39 +1100

    Code commenting and clean up

--- SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Controller/Controller_StudentMenu.java
+++ SEPPrototype/app/src/main/java/uts/sep/tcba/sepprototype/Controller/Controller_StudentMenu.java
@@ -32,47 +32,49 @@ import uts.sep.tcba.sepprototype.Model.Student;
 
 public class Controller_StudentMenu extends AppCompatActivity{
 
-private TextView mTextMessage;
-private ListView listView;
-private FloatingActionButton newBookingButton;
-private LinkedList<Booking> bookings = new LinkedList<Booking>();
-private LinkedList<String> subjects = new LinkedList<String>();
-private LinkedList<Notification> notifications = new LinkedList<Notification>();
-private LinkedList<String> pageList = new LinkedList<String>();
-private ArrayAdapter adapter;
-private FirebaseAuth mAuth;
-private Student currentStudent;
-private boolean bookingTab = true;
-private boolean subjectTab = false;
-private boolean notifTab = false;
-final Context context = this; //TODO: not sure what this is doing
-private boolean newBooking = true;
+    private TextView mTextMessage;
+    private ListView listView;
+    private FloatingActionButton newBookingButton;
+    private LinkedList<Booking> bookings = new LinkedList<Booking>();
+    private LinkedList<String> subjects = new LinkedList<String>();
+    private LinkedList<Notification> notifications = new LinkedList<Notification>();
+    private LinkedList<String> pageList = new LinkedList<String>();
+    private ArrayAdapter adapter;
+    private FirebaseAuth mAuth;
+    private Student currentStudent;
+    private boolean bookingTabSelected = true;
+    private boolean subjectTabSelected = false;
+    private boolean notifTabSelected = false;
+    final Context context = this; //TODO: not sure what this is doing
+    private boolean newBooking = true;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
             new BottomNavigationView.OnNavigationItemSelectedListener(){
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item){
-                    bookingTab = false;
-                    subjectTab = false;
-                    notifTab = false;
+                    // Reset tab selected boolean flags
+                    bookingTabSelected = false;
+                    subjectTabSelected = false;
+                    notifTabSelected = false;
+                    // Depending on the tab selected
                     switch(item.getItemId()){
-                        case R.id.navigation_home:
-                            bookingTab = true;
-                            mTextMessage.setText(R.string.bookings_placeholder);
-                            newBookingButton.setVisibility(View.VISIBLE);
-                            refreshListView();
+                        case R.id.navigation_home: // if the booking tab is selected
+                            bookingTabSelected = true; // set booking tab flag
+                            mTextMessage.setText(R.string.bookings_placeholder); // set placeholder text if no bookings to show
+                            newBookingButton.setVisibility(View.VISIBLE); // enable new booking
+                            refreshListView(); // refresh list UI
                             return true;
-                        case R.id.navigation_dashboard:
-                            subjectTab = true;
-                            mTextMessage.setText(R.string.subjects_placeholder);
-                            newBookingButton.setVisibility(View.GONE);
-                            refreshListView();
+                        case R.id.navigation_dashboard: // if the subject tab is selected
+                            subjectTabSelected = true; // set subject tab flag
+                            mTextMessage.setText(R.string.subjects_placeholder); // set placeholder text if no subjects to show
+                            newBookingButton.setVisibility(View.GONE); // disable new booking
+                            refreshListView(); // refresh list UI
                             return true;
-                        case R.id.navigation_notifications:
-                            notifTab = true;
-                            mTextMessage.setText(R.string.notifications_placeholder);
-                            newBookingButton.setVisibility(View.GONE);
-                            refreshListView();
+                        case R.id.navigation_notifications: // if the notification tab is selected
+                            notifTabSelected = true; // set notification tab flag
+                            mTextMessage.setText(R.string.notifications_placeholder); // set placeholder text if notifications to show
+                            newBookingButton.setVisibility(View.GONE); // disable new booking
+                            refreshListView(); // refresh list UI
                             return true;
                     }
                     return false;
@@ -82,7 +84,7 @@ private boolean newBooking = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // Activity Initialisation
+        // Activity initialisation
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentmenu);
         mTextMessage = (TextView) findViewById(R.id.message);
@@ -90,18 +92,9 @@ private boolean newBooking = true;
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_student);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        // Fetch user from previous activity
-        Intent intent = getIntent();
-        int loggedInUserID = Integer.parseInt(intent.getStringExtra("user"));
-        currentStudent = new Student(loggedInUserID);
-
-        initFirebase();
-
-        // Set ListView UI element to display different lists based on tab selected
-        listView = (ListView) findViewById(R.id.list);
-
-        // Define a new Adapter to bind data to UI list
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pageList) {
+        // Data ListView initialisation
+        listView = (ListView) findViewById(R.id.list); // Set ListView UI element to display different lists based on tab selected
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pageList) { // Define a new Adapter to bind data to UI list
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
@@ -113,16 +106,11 @@ private boolean newBooking = true;
                 return view;
             }
         };
-
-        // Bind the adapted to the UI list
-        listView.setAdapter(adapter);
-
-        // Set a listener for clicking item in the ListView to trigger the view booking screen on the pressed list item
+        listView.setAdapter(adapter); // Bind the adapted to the UI list
         listView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) { // Set a listener for clicking item in the ListView to trigger the view booking screen on the pressed list item
                 BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_student);
-                if (bookingTab) {
+                if (bookingTabSelected) {
                     Intent intent = new Intent(Controller_StudentMenu.this, Controller_ViewBooking.class);
                     Booking selectedItem = bookings.get(position);
                     intent.putExtra("booking", selectedItem);
@@ -134,7 +122,7 @@ private boolean newBooking = true;
             }
         });
 
-        // Define the new booking button in the bottom right
+        // Initialise the new booking button in the bottom right
         newBookingButton = (FloatingActionButton) findViewById(R.id.newBooking);
         newBookingButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -145,6 +133,13 @@ private boolean newBooking = true;
                 startActivityForResult(intent, 1);
             }
         });
+
+        // User initialisation
+        int loggedInUserID = Integer.parseInt(getIntent().getStringExtra("user")); // Fetch user from previous activity
+        currentStudent = new Student(loggedInUserID); // create student object from user ID (constructor pulls data directly from Firebase)
+
+        // Firebase data change listeners initialised and data fetched
+        initFirebase();
     }
 
     public void initFirebase() {
@@ -166,7 +161,7 @@ private boolean newBooking = true;
         refUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                setTitle(getTitle() + " - " + currentStudent.getFullName() + " (S)");
+                setTitle(getTitle() + " - " + currentStudent.getFullName() + " (S)"); // set title of menu to include user's name
                 subjects.addAll(currentStudent.getSubjects()); // Populate user subjects
                 for (DataSnapshot d : dataSnapshot.child("Notifications").getChildren()) {
                     notifications.add(new Notification(d)); //  get all notifications for user
@@ -183,7 +178,7 @@ private boolean newBooking = true;
     public void getBookings(DataSnapshot dataSnapshot) {
         bookings.clear(); // clears currently stored bookings
         for (DataSnapshot booking : dataSnapshot.getChildren()) { // for each booking
-            //Log.d("GetBooking", booking.toString());
+            Log.i("Booking", booking.toString()); // informational debug statement written to log indicating a booking fetched
             if (booking.child("students").child(String.valueOf(currentStudent.getID())).exists()) { // if the the logged in tutor is the tutor hosting the booking
                 bookings.add(new Booking(booking)); // create a new booking object to be stored locally
             }
@@ -192,7 +187,7 @@ private boolean newBooking = true;
         refreshListView(); // refresh list UI
     }
 
-    public void sortBookings() {
+    public void sortBookings() { // chronologically sorts bookings based on start time
         Collections.sort(bookings, new Comparator<Booking>() {
             public int compare(Booking b1, Booking b2) {
                 DateFormat formatter = new SimpleDateFormat("dd/MM/yy HH:mm");
@@ -208,15 +203,15 @@ private boolean newBooking = true;
         });
     }
 
-    public void refreshListView() {
+    public void refreshListView() { // reload the listview data on tab change
         pageList.clear(); // clear the list
-        if (bookingTab) { // if the bookings tab is the active tab
+        if (bookingTabSelected) { // if the bookings tab is the active tab
             for (Booking b : bookings) {
                 pageList.add(b.toString()); // load bookings into the list
             }
-        } else if (subjectTab) { // if the subjects tab is the active tab
+        } else if (subjectTabSelected) { // if the subjects tab is the active tab
             pageList.addAll(subjects); // load subjects into the list
-        } else if (notifTab) { // if the notifications tab is the active tab
+        } else if (notifTabSelected) { // if the notifications tab is the active tab
             for (Notification not : notifications) {
                 pageList.add(not.cancelledMessageToStudent());  // load notifications into the list
             }
@@ -231,22 +226,23 @@ private boolean newBooking = true;
 
     public void addBookingToFirebase(final Booking booking, String existingBookingID) {
         DatabaseReference bookingsRef = FirebaseDatabase.getInstance().getReference("Bookings"); // get reference of bookings in Firebase
-        DatabaseReference newBookingRef = bookingsRef.push(); // get new reference
+        DatabaseReference newBookingRef = bookingsRef.push(); // get the database reference for new booking
         DatabaseReference targetRef;
-        if (newBooking) {
-            newBookingRef.setValue(booking);
-            targetRef = newBookingRef;
-        } else {
-            targetRef = bookingsRef.child(existingBookingID);
+        if (newBooking) { // if its a new booking (user not being added to an existing booking object in Firebase)
+            newBookingRef.setValue(booking); // push the booking object to Firebase
+            targetRef = newBookingRef; // set the target ref to the location of the new booking object
+        } else { // otherwise being added to an existing booking object in Firebase
+            targetRef = bookingsRef.child(existingBookingID); // set the target ref to the location of the existing booking
         }
-        setCurrentStudentInformation(targetRef, booking.getCurrentStudentNotes()); // adds current student and their notes to the booking
+        setCurrentStudentInformation(targetRef, booking.getCurrentStudentNotes()); // adds current student and their notes to the targeted booking
     }
 
     public void setCurrentStudentInformation(DatabaseReference bookingStudentStatus, String studentNotes) {
-        bookingStudentStatus.child("students").child(String.valueOf(currentStudent.getID())).child("Name").setValue(currentStudent.getFullName());
-        bookingStudentStatus.child("students").child(String.valueOf(currentStudent.getID())).child("Notes").setValue(studentNotes);
+        bookingStudentStatus.child("students").child(String.valueOf(currentStudent.getID())).child("Name").setValue(currentStudent.getFullName()); // adds student to booking
+        bookingStudentStatus.child("students").child(String.valueOf(currentStudent.getID())).child("Notes").setValue(studentNotes); // adds student's notes to booking
     }
 
+    // Handle log out
     @Override
     public void onBackPressed(){
         AlertDialog alertDialog = new AlertDialog.Builder(Controller_StudentMenu.this).create();
@@ -264,30 +260,28 @@ private boolean newBooking = true;
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         mAuth = FirebaseAuth.getInstance();
-                        mAuth.signOut();
-                        finish();
+                        mAuth.signOut(); // sign user out of application
+                        finish(); // return to login activity
                     }
                 });
         alertDialog.show();
     }
 
-    // Return method from make booking view
+    // Return method from MakeBooking controller
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && data != null) {
+        if (requestCode == 1 && data != null) { // if the activity returning to the StudentMenu is MakeBooking
             if (resultCode == RESULT_OK) {
-                Bundle bundle = data.getExtras();
-                String existingBookingID = bundle.getString("existingBookingID");
-                if (existingBookingID.equals("")) {
-                    Log.d("IT'S", "NULL");
+                Bundle bundle = data.getExtras(); // extract data passed back
+                String existingBookingID = bundle.getString("existingBookingID"); // get the existingBookingID if there is one
+                if (existingBookingID.equals("")) { // if there isn't an existing booking
                     newBooking = true;
-                } else {
-                    Log.d("IT'S", "NOT");
+                } else { // otherwise there is an existing booking for the student to be added to
                     newBooking = false;
                 }
-                Booking booking = (Booking) bundle.getSerializable("booking");
-                addBookingToFirebase(booking, existingBookingID);
-                sortBookings();
-                adapter.notifyDataSetChanged();
+                Booking booking = (Booking) bundle.getSerializable("booking"); // extract booking object from makeBooking controller
+                addBookingToFirebase(booking, existingBookingID); // save the relevant information to Firebase
+                sortBookings(); // sort the bookings chronologically
+                adapter.notifyDataSetChanged(); // notify the listview to refresh the UI
             }
         }
     }
